c9334c960287b704bdff1d1d32ff261c9ddc24bc
Delan Azabani <delan@azabani.com>
2019-11-27 16:59:45 +1100

    fix ERR_HTTP_HEADERS_SENT when a rate limit is exceeded

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,14 +12,11 @@ export default function middleware(
     // should™ be monotonic <https://stackoverflow.com/a/46964780>
     const delay = limiter.delay(req.ip, process.hrtime.bigint());
 
-    if (delay != null) {
-      res
-        .status(429)
-        .send(
-          `Rate limit exceeded. Try again in ${delay / 1_000_000_000n} seconds`,
-        );
+    if (delay == null) {
+      return void next();
     }
 
-    next();
+    res.status(429).send(`Rate limit exceeded. Try again in ${delay / 1_000_000_000n} seconds`);
+    console.warn(`rate limit exceeded for ${req.ip} (HTTP 429; wait ${delay} ns)`)
   };
 }
